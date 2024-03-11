b6606fae41a3577497a698a550fbdf6fd4913dbe
Franz Mamani <retibol.bolivia@gmail.com>
2024-03-11 19:11:45 -0400

    inicio

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente{
+  icon:string;
+  name:string;
+  redirectTo:string;
+}
+
+
+
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[]=[
+    {
+      icon:'american-football',
+      name:'Action-sheet',
+      redirectTo:'/action-sheet'
+    },
+    {
+      icon:'logo-apple-appstore',
+      name:'Alert',
+      redirectTo:'/alert'
+    }
+  ];
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
